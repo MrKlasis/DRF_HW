57b4e84a565910d0e574f3c01e6d3853cf74bbf2
Klasis <d23kv321@gmail.com>
2024-02-08 21:05:52 +0300

    fix views\course.py

--- school/views/course.py
+++ school/views/course.py
@@ -1,43 +1,19 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework import viewsets
+from school.models import Course
+from school.seriallizers.course import CourseSerializer
+from school.permissions import IsOwner, IsModerator
 from rest_framework.permissions import IsAuthenticated
 
-from school.models import Course
-from school.paginators import CoursePaginator
-from school.permissions import IsOwner, IsModerator
-from school.seriallizers.course import CourseSerializer
 
-
-class CourseCreateAPIView(CreateAPIView):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        new_course = serializer.save()
-        new_course.owner = self.request.user
-        new_course.save()
+        serializer.save(owner=self.request.user)
 
-
-class CourseListAPIView(ListAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = CoursePaginator
-
-
-class CourseRetrieveAPIView(RetrieveAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    permission_classes = [IsOwner | IsModerator]
-
-
-class CourseUpdateAPIView(UpdateAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    permission_classes = [IsOwner | IsModerator]
-
-
-class CourseDestroyAPIView(DestroyAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    permission_classes = [IsOwner]
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsOwner | IsModerator]
+        return super().get_permissions()
